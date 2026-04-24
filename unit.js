fc17f863aac309726f480d40b1b492296b80785f
indie-indie <rikutoinda@gmail.com>
2026-04-24 17:12:23 +0900

    Add files via upload

--- unit.js
+++ unit.js
@@ -31,88 +31,88 @@ var AFFINITY_BONUS_DEFAULT = 1.5;
 
 var PLAYER_UNITS = {
   nico: {
-    n:'ニコ', e:'😄', cost:20, hp:100, dmg:13, spd:40, rng:30, ar:1.0,
+    n:'ジェームズ', e:'😄', cost:20, hp:100, dmg:13, spd:40, rng:30, ar:1.0,
     type:'ground', targets:'ground', cd:1.0, size:0.8, rew:1, zone:'own', unlockCost:0,
     attr:'beast', affinity:null, crt:1.5
   },
   hoko: {
-    n:'ホッコ', e:'😊', cost:25, hp:80, dmg:20, spd:30, rng:30, ar:1.0,
+    n:'ハリー', e:'😊', cost:25, hp:80, dmg:20, spd:30, rng:30, ar:1.0,
     type:'ground', targets:'ground', cd:2.0, size:1.0, rew:1, zone:'own', unlockCost:0,
     attr:'beast', affinity:null, crt:1.5,
     skills: [{ trigger:'onDeath', effect:'split' }]
   },
   fuu: {
-    n:'フゥ', e:'😤', cost:30, hp:50, dmg:10, spd:100, rng:40, ar:0.75,
+    n:'ジョン', e:'😤', cost:30, hp:50, dmg:10, spd:100, rng:40, ar:0.75,
     type:'ground', targets:'ground', cd:1.0, size:0.8, rew:1.5, zone:'own', unlockCost:0,
     attr:'beast', affinity:null, crt:1.5
   },
   ikari: {
-    n:'イカリ', e:'😠', cost:40, hp:150, dmg:30, spd:60, rng:50, ar:1.0,
+    n:'マイケル', e:'😠', cost:40, hp:150, dmg:30, spd:60, rng:50, ar:1.0,
     type:'ground', targets:'ground', cd:1.5, size:1.1, rew:1.2, zone:'own', unlockCost:0,
     attr:'humanoid', affinity:'beast', crt:1.5
   },
   teko: {
-    n:'テコ', e:'🤓', cost:30, hp:80, dmg:15, spd:50, rng:120, ar:0.75,
+    n:'デイビッド', e:'🤓', cost:30, hp:80, dmg:15, spd:50, rng:120, ar:0.75,
     type:'ground', targets:'both', cd:1.0, size:0.9, rew:1, zone:'own', unlockCost:0,
     attr:'humanoid', affinity:null, crt:1.5
   },
   noo: {
-    n:'ノー', e:'😐', img:'nyanko-game/image/golem', cost:60, hp:500, dmg:15, spd:25, rng:30, ar:1.5,
+    n:'ロバート', e:'😐', cost:60, hp:500, dmg:15, spd:25, rng:30, ar:1.5,
     type:'ground', targets:'ground', isBase:true, cd:2.5, size:1.5, rew:2, zone:'own', unlockCost:250,
     attr:'construct', affinity:'humanoid', crt:1.8
   },
   jiro: {
-    n:'ジロ', e:'🧐', cost:40, hp:50, dmg:40, spd:40, rng:150, ar:1.6,
+    n:'ウィリアム', e:'🧐', cost:40, hp:50, dmg:40, spd:40, rng:150, ar:1.6,
     type:'ground', targets:'both', cd:3.0, size:0.9, rew:1, zone:'own', unlockCost:250,
     attr:'humanoid', affinity:'machine', crt:2.0,
     skills: [{ trigger:'onHit', effect:'pierce' }]
   },
   guru: {
-    n:'グル', e:'😵‍💫', cost:50, hp:30, dmg:8, spd:20, rng:80, ar:0.3,
+    n:'リチャード', e:'😵‍💫', cost:50, hp:30, dmg:8, spd:20, rng:80, ar:0.3,
     type:'ground', targets:'both', cd:4.0, size:0.7, rew:1, zone:'own', unlockCost:250,
     attr:'construct', affinity:'beast', crt:1.5,
     skills: [{ trigger:'onHit', effect:'pierce' }]
   },
   waru: {
-    n:'ワル', e:'😈', cost:35, hp:80, dmg:90, spd:90, rng:20, ar:0.8,
+    n:'トーマス', e:'😈', cost:35, hp:80, dmg:90, spd:90, rng:20, ar:0.8,
     type:'ground', targets:'ground', cd:5.0, size:0.8, rew:1, zone:'all', unlockCost:350,
     attr:'humanoid', affinity:'humanoid', crt:1.6,
     skills: [{ trigger:'onHit', effect:'poison' }]
   },
   don: {
-    n:'ドン', e:'🤯', cost:50, hp:120, dmg:100, spd:30, rng:120, ar:1.5, area:10,
+    n:'チャールズ', e:'🤯', cost:50, hp:120, dmg:100, spd:30, rng:120, ar:1.5, area:10,
     type:'ground', targets:'both', cd:5.5, size:1.0, rew:1, zone:'own', unlockCost:400,
     attr:'humanoid', affinity:'undead', crt:2.0
   },
   biri: {
-    n:'ビリ', e:'🤩', cost:60, hp:90, dmg:30, spd:30, rng:120, ar:1.2, area:5,
+    n:'ジョージ', e:'🤩', cost:60, hp:90, dmg:30, spd:30, rng:120, ar:1.2, area:5,
     type:'ground', targets:'both', cd:6.5, size:1.0, rew:1, zone:'own', unlockCost:400,
     attr:'construct', affinity:'machine', crt:2.0,
     skills: [{ trigger:'onHit', effect:'thunder' }]
   },
   muku: {
-    n:'ムク', e:'😶', cost:20, hp:60, dmg:20, spd:80, rng:50, ar:0.6,
+    n:'エドワード', e:'😶', cost:20, hp:60, dmg:20, spd:80, rng:50, ar:0.6,
     type:'ground', targets:'ground', cd:1.0, size:0.9, rew:2, zone:'own', unlockCost:200,
     attr:'beast', affinity:null, crt:1.5
   },
   niya: {
-    n:'ニヤ', e:'😏', cost:100, hp:400, dmg:50, spd:50, rng:110, ar:1.5,
+    n:'ヘンリー', e:'😏', cost:100, hp:400, dmg:50, spd:50, rng:110, ar:1.5,
     type:'air', targets:'base', cd:10.0, size:1.4, rew:3, zone:'own', unlockCost:400,
     attr:'beast', affinity:'humanoid', crt:1.7
   },
   buchi: {
-    n:'ブチ', e:'🤬', cost:50, hp:180, dmg:50, spd:30, rng:130, ar:2.0, area:50,
+    n:'フレデリック', e:'🤬', cost:50, hp:180, dmg:50, spd:30, rng:130, ar:2.0, area:50,
     type:'ground', targets:'ground', cd:5.0, size:1.2, rew:1, zone:'own', unlockCost:500,
     attr:'construct', affinity:'undead', crt:1.8
   },
   kaa: {
-    n:'カァ', e:'😡', cost:100, hp:250, dmg:80, spd:30, rng:100, ar:1.5, area:40,
+    n:'ダニエル', e:'😡', cost:100, hp:250, dmg:80, spd:30, rng:100, ar:1.5, area:40,
     type:'air', targets:'both', cd:10.0, size:1.3, rew:1, zone:'own', unlockCost:700,
     attr:'beast', affinity:'machine', crt:2.0,
     skills: [{ trigger:'onHit', effect:'freeze' }]
   },
   akuma: {
-    n:'アクマ', e:'👿', cost:500, hp:1000, dmg:300, spd:8, rng:100, ar:5.0, area:80,
+    n:'ベンジャミン', e:'😎', cost:500, hp:1000, dmg:300, spd:8, rng:100, ar:5.0, area:80,
     type:'ground', targets:'both', cd:30.0, size:2.0, rew:1, zone:'own', unlockCost:1000,
     attr:'beast', affinity:null, crt:1.5
   },
